36ed87b41911c78432c63e49a76d9ef2cf78b072
xkamson <marcinxkaminski@gmail.com>
2019-01-16 17:59:23 +0100

    Fixed generator

--- bricks_generator.ads
+++ bricks_generator.ads
@@ -1,12 +1,8 @@
 with Ada.Real_Time, Ada.Numerics, Ada.Numerics.Discrete_Random, Bricks_Generator_Action, Bricks;
 use Ada.Real_Time, Bricks_Generator_Action, Bricks;
-with buffer;
 
 package Bricks_Generator is
-    package BrickBuffer is new buffer(max => 3, element_type => Brick);
-
     procedure Get(b: in out Brick);
-
     procedure TerminateGenerator;    
 
     private
--- bricks_generator_action.adb
+++ bricks_generator_action.adb
@@ -1,18 +1,35 @@
 package body Bricks_Generator_Action is
   protected body Action is
-    procedure Add(b: in Brick; is_buf_full: in out boolean) is
+    procedure Add(b: in Brick; i: in out boolean) is
     begin
       if not is_full then
-        BrickBuffer.CircularBuffer.Add(b);
-        is_buf_full := is_full;
+        BrickBuffer.CircularBuffer.add(b);
+        is_empty := false;
+        capacity := capacity + 1;
+
+        if(capacity < 3) then
+          is_full := false;
+        else
+          is_full := true;
+        end if;
+        
+        i := is_full;
       end if;
     end Add;
 
-    entry Get (b: in out Brick; is_buf_full: in out boolean) when not is_empty is
+    entry Get (b: in out Brick; i: in out boolean) when not is_empty is
     begin
-        -- buf.Get(b, is_full, is_empty);
         BrickBuffer.CircularBuffer.get(b);
-        is_buf_full := is_full;
+        is_full := false;
+        i := false;
+        capacity := capacity - 1;
+
+        if(capacity > 0) then
+          is_empty := false;
+        else
+          is_empty := true;
+        end if;
+
     end Get;
   end Action;
 
